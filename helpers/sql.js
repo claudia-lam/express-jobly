4d2f6a5e09b694e07f0c2935e91047956061be44
Justin Clark <jclarksummit@gmail.com>
2023-04-12 14:38:09 -0400

    Update docstring

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,19 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Helper function to format JSON body from .update routes for SET sql
+ * statement
+ *
+ * Takes:
+ *  - dataToUpdate: JS object containing data meant to DB
+ *  - jsToSql: JS object containing column names for DB model
+ *
+ * Returns:
+ * {
+ *   setCols: (string of column names for SET statement),
+ *   values: [array of values for SET statement]
+ * }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -8,7 +21,7 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
