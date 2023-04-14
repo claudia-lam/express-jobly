215978f51855f36a95ada14f105843799736d1f8
Justin Clark <jclarksummit@gmail.com>
2023-04-14 18:28:10 -0400

    Add test values for jobs table

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -24,10 +24,17 @@ async function commonBeforeAll() {
         VALUES ('u1', $1, 'U1F', 'U1L', 'u1@email.com'),
                ('u2', $2, 'U2F', 'U2L', 'u2@email.com')
         RETURNING username`,
-      [
-        await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
-        await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
-      ]);
+    [
+      await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
+      await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
+    ]);
+
+  await db.query(`
+  INSERT INTO jobs(title, salary, equity, company_handle)
+  VALUES ('j1', '100000', 0, 'c1'),
+         ('j2', '90000', 0.01, 'c2'),
+         ('j3', '50000', 0.03, 'c3')
+  `);
 }
 
 async function commonBeforeEach() {
